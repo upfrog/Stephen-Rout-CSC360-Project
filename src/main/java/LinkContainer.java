a070755d4b2b4007638af9ea7825b0dc3fe1eb45
upfrog <srr@routhouse.org>
2024-03-13 02:46:05 -0400

    Add pre-existing work for Structure, LinkContainer

---
+++ src/main/java/LinkContainer.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class LinkContainer
+{
+	HashMap<String, ArrayList<Structure>> linkMap;
+	
+	public LinkContainer()
+	{
+		linkMap = new HashMap<String, ArrayList<Structure>>();
+	}
+	
+	public boolean contains(String listType, Structure object)
+	{
+		return linkMap.get(listType).contains(object);
+	}
+	
+	public void addLink(String listType, Structure object)
+	{
+		linkMap.get(listType).add(object);
+	}
+	
+	public void removeLink(String listType, Structure object)
+	{
+		linkMap.get(listType).remove(object);
+	}
+	
+	public ArrayList<Structure> getList(String listType)
+	{
+		return linkMap.get(listType);
+	}
+	
+	public int getListLength(String listType)
+	{
+		return linkMap.get(listType).size();
+	}
+	
+	
+	
+}
--- src/main/java/Structure.java
+++ src/main/java/Structure.java
@@ -1,19 +1,17 @@
-/**
- * 
- */
+import java.time.LocalDateTime;
 
-/**
- * 
- */
 public abstract class Structure
 {
+	final int UID;
+	final LocalDateTime creationDateTime;
+	LinkContainer links;
+	
 
-	/**
-	 * 
-	 */
 	public Structure()
 	{
-		// TODO Auto-generated constructor stub
+		this.UID = 3;
+		this.creationDateTime = LocalDateTime.now();
+		this.links = new LinkContainer();
 	}
 
 }
